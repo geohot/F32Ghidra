61b07c70a358d1c64898142fffa53dbbc5344991
George Hotz <geohot@gmail.com>
2025-04-05 17:48:30 +0800

    fix for my system

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -xeo pipefail
 rm -rf dist/*
+export GHIDRA_INSTALL_DIR=$HOME/src/ghidra_11.2_PUBLIC/
 gradle buildExtension
 FILE="$PWD/dist/*"
-cd ~/Library/ghidra/ghidra_11.3.1_PUBLIC/Extensions
+mkdir ~/Library/ghidra/ghidra_11.2_PUBLIC/Extensions
+cd ~/Library/ghidra/ghidra_11.2_PUBLIC/Extensions
 rm -rf F32Ghidra
 unzip $FILE
-~/src/ghidra_11.3.1_PUBLIC/ghidraRun
+~/src/ghidra_11.2_PUBLIC/ghidraRun
